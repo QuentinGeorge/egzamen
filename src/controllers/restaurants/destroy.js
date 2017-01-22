b3c2dab613609303240a3ec5a2e2ffa60fa23b9f
Quentin George <quentingeorge7@gmail.com>
2017-01-22 22:58:53 +0100

    Add restaurant destroy

---
+++ src/controllers/restaurants/destroy.js
@@ -0,0 +1,35 @@
+/** Exam RIA - 25/01/2017
+*** QuentinGeorge/egzamen
+***
+*** /src/controllers/restaurants/destroy.js - Delete restaurants controller
+***
+*** Coded by Quentin George
+**/
+
+import { ObjectID } from "mongodb";
+import { send, error } from "../../core/utils/api";
+import getRestaurants from "../../models/restaurants";
+
+export default function( oRequest, oResponse ) {
+
+    let oRestaurantID;
+
+    try {
+        oRestaurantID = new ObjectID( oRequest.params.id );
+    } catch ( oError ) {
+        return error( oRequest, oResponse, new Error( "Invalid ID!" ), 400 );
+    }
+
+    getRestaurants()
+        .deleteOne( {
+            "_id": oRestaurantID,
+        } )
+        .then( ( { deletedCount } ) => {
+            if ( deletedCount === 1 ) {
+                return send( oRequest, oResponse, null, 204 );
+            }
+
+            return error( oRequest, oResponse, "Unknown deletion error", 500 );
+        } )
+        .catch( ( oError ) => error( oRequest, oResponse, oError ) );
+}
--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -12,7 +12,7 @@ import list from "../controllers/restaurants/list";
 import details from "../controllers/restaurants/details";
 import create from "../controllers/restaurants/create";
 // import update from "../controllers/restaurants/update";
-// import destroy from "../controllers/restaurants/destroy";
+import destroy from "../controllers/restaurants/destroy";
 
 let oRouter = new Router();
 
@@ -20,6 +20,6 @@ oRouter.get( "/restaurants", list );
 oRouter.get( "/restaurants/:id", details );
 oRouter.post( "/restaurants", create );
 // oRouter.patch( "/restaurants/:id", update );
-// oRouter.delete( "/restaurants/:id", destroy );
+oRouter.delete( "/restaurants/:id", destroy );
 
 export default oRouter;
