6fb14a69b1cc7c073f8c7589e769dbf09c56759d
Quentin George <quentingeorge7@gmail.com>
2017-01-23 22:41:38 +0100

    Fix create controller

--- src/controllers/restaurants/create.js
+++ src/controllers/restaurants/create.js
@@ -6,7 +6,6 @@
 *** Coded by Quentin George
 **/
 
-import { ObjectID } from "mongodb";
 import getRestaurants from "../../models/restaurants";
 import { send, error } from "../../core/utils/api";
 import checkPosition from "../../core/utils/position";
@@ -21,49 +20,16 @@ export default function( oRequest, oResponse ) {
         sSlug = sName.toLowerCase().replace( " ", "-" ),
         sAddress = ( POST.address || "" ).trim(),
         oPosition = checkPosition( iLatitude, iLongitude ),
-        aHours = [],
+        aHours = POST.hours,
         oRestaurant;
 
     if ( !oPosition ) {
         return error( oRequest, oResponse, "Invalid position", 400 );
     }
 
-    aHours = [
-      [
-        POST.monday_opening,
-        POST.monday_closing
-      ],
-      [
-        POST.tuesday_opening,
-        POST.tuesday_closing
-      ],
-      [
-        POST.wednesday_opening,
-        POST.wednesday_closing
-      ],
-      [
-        POST.thursday_opening,
-        POST.thursday_closing
-      ],
-      [
-        POST.friday_opening,
-        POST.friday_closing
-      ],
-      [
-        POST.saturday_opening,
-        POST.saturday_closing
-      ],
-      [
-        POST.sunday_opening,
-        POST.sunday_closing
-      ]
-    ];
-
     oRestaurant = {
         "latitude": oPosition.latitude,
         "longitude": oPosition.longitude,
-        "created_at": new Date(),
-        "updated_at": new Date(),
     };
 
     sName && ( oRestaurant.name = sName );
@@ -75,7 +41,6 @@ export default function( oRequest, oResponse ) {
         .insertOne( oRestaurant )
         .then( () => {
             send( oRequest, oResponse, {
-                "id": oRestaurant._id,
                 "slug": oRestaurant.slug,
                 "name": oRestaurant.name || null,
                 "address": oRestaurant.address || null,
