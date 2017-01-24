efb59f348a1537f5e02c4bcc4e13e7418e9852d8
Quentin George <quentingeorge7@gmail.com>
2017-01-24 21:51:22 +0100

    Page details fix display

--- static/modules/components/restaurants/details.js
+++ static/modules/components/restaurants/details.js
@@ -29,32 +29,70 @@ let oRestaurantDetails = Vue.component( "restaurant-details", {
                 </p>
             </div>
             <div v-if="loaded">
-                <h2>{{ restaurant ? restaurant.name : "Unknown" }}</h2>
-                <address>{{ restaurant.address }}</address>
-                <p>{{ restaurant.distance }}m</p>
+                <h2>{{ restaurant.name ? restaurant.name : "Unknown" }}<span> ({{ restaurant.slug }})</span></h2>
+                <address>Address&nbsp;: {{ restaurant.address }}</address>
+                <p>Latitude&nbsp;: {{ restaurant.latitude }}</p>
+                <p>Longitude&nbsp;: {{ restaurant.longitude }}</p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td><strong>Ouverture</strong></td>
+                            <td><strong>Fermeture</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Lundi</strong></td>
+                            <td>{{ restaurant.hours[ 0 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 0 ][ 1 ] }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Mardi</strong></td>
+                            <td>{{ restaurant.hours[ 1 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 1 ][ 1 ] }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Mercredi</strong></td>
+                            <td>{{ restaurant.hours[ 2 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 2 ][ 1 ] }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Jeudi</strong></td>
+                            <td>{{ restaurant.hours[ 3 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 3 ][ 1 ] }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Vendredi</strong></td>
+                            <td>{{ restaurant.hours[ 4 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 4 ][ 1 ] }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Samedi</strong></td>
+                            <td>{{ restaurant.hours[ 5 ][ 0 ] }}</td>
+                            <td>{{ restaurant.hours[ 5 ][ 1 ] }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p v-if="!restaurant.open">Fermer</p>
+                <p v-if="restaurant.open">Ouvert</p>
             </div>
         </div>
     `,
+    mounted() {
+        this.fetchInfos( this.$route.params.id );
+    },
     "methods": {
         fetchInfos( sRestaurantId ) {
-            return getLocation()
-                .then( ( { coords } ) => {
-                    return reqwest( {
-                        "url": `/restaurants/${ sRestaurantId }`,
-                        "method": "get",
-                        "data": {
-                            "latitude": coords.latitude,
-                            "longitude": coords.longitude,
-                        },
-                    } );
-                } )
-                .then( ( oResponse ) => {
-                    let oRestaurant = oResponse.data;
-
-                    this.loaded = true;
-                    this.restaurant = oRestaurant;
-                } )
-                .catch( this.showError );
+            reqwest( {
+                "url": `/restaurants/${ sRestaurantId }`,
+                "method": "get",
+                "data": {},
+            } )
+            .then( ( oResponse ) => {
+                this.restaurant = oResponse.data;
+                console.log(this.restaurant.hours);
+                this.loaded = true;
+            } )
+            .catch( this.showError );
         },
         showError( { message } ) {
             this.loaded = true;
